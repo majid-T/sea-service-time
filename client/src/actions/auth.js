00e7c36aefa703f7ea5e06a4c59ae47f06225009
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-08-10 23:54:28 +0430

    typo on action fixed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -40,7 +40,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     },
   };
 
-  const body = JSON.stringify({ name, email, password,role='seafarer' });
+  const body = JSON.stringify({ name, email, password, role: "seafarer" });
 
   try {
     const res = await axios.post("/api/users", body, config);
