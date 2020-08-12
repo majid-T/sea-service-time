5946635acd8fb23a14ce0ce7c07d69673b050a8e
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-08-13 00:56:00 +0430

    Added records mappping

--- client/src/components/dashboard/seafarer/SeaRecordCard.js
+++ client/src/components/dashboard/seafarer/SeaRecordCard.js
@@ -3,15 +3,31 @@ import React from "react";
 const SeaRecordCard = ({ userRecord }) => {
   return (
     <>
-      <h1>New Show record</h1>
-      <p>{userRecord.name}</p>
-      <p>{userRecord.cdn}</p>
-      <p>{userRecord.dateOfBirth}</p>
-      <p>{userRecord.dateOfBirth}</p>
-      <p>{userRecord.rank}</p>
-      <p>{userRecord.recordId}</p>
-      <p>{userRecord.seaTime}</p>
-      <p>{userRecord.status}</p>
+      {userRecord && (
+        <>
+          <h1>New Show record</h1>
+          <p>{userRecord.name}</p>
+          <p>{userRecord.cdn}</p>
+          <p>{userRecord.dateOfBirth}</p>
+          <p>{userRecord.dateOfBirth}</p>
+          <p>{userRecord.rank}</p>
+          <p>{userRecord.recordId}</p>
+          <p>{userRecord.seaTime}</p>
+          <p>{userRecord.status}</p>
+          <h3>Records are:</h3>
+          <hr />
+          {userRecord.serviceTimes &&
+            userRecord.serviceTimes.map((res, index) => (
+              <div key={index}>
+                <span>{res.time} | </span>
+                <span>{res.dateSignIn} | </span>
+                <span>{res.dateSignOff} | </span>
+                <span>{res.vesselNo} | </span>
+                <span>{res.vesselOwner} | </span>
+              </div>
+            ))}
+        </>
+      )}
     </>
   );
 };
