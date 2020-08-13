e6533af055944a7785acc70e47905ac85761495d
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-08-13 21:38:55 +0430

    CSS on record card part 1

--- client/src/components/dashboard/seafarer/SeaRecordCard.js
+++ client/src/components/dashboard/seafarer/SeaRecordCard.js
@@ -1,32 +1,68 @@
 import React from "react";
+import Moment from "react-moment";
 
 const SeaRecordCard = ({ userRecord }) => {
+  const seaRecords = userRecord.serviceTimes.map((res, index) => {
+    return (
+      <tr key={index}>
+        <td className="hide-sm">{index}</td>
+        <td>{res.vesselNo}</td>
+        <td>{res.vesselOwner}</td>
+        <td>
+          <Moment format="YYYY/MM/DD">{res.dateSignIn}</Moment> -
+          <Moment format="YYYY/MM/DD">{res.dateSignOff}</Moment>
+        </td>
+        <td>{res.time}</td>
+      </tr>
+    );
+  });
   return (
     <>
       {userRecord && (
-        <>
-          <h1>New Show record</h1>
-          <p>{userRecord.name}</p>
-          <p>{userRecord.cdn}</p>
-          <p>{userRecord.dateOfBirth}</p>
-          <p>{userRecord.dateOfBirth}</p>
-          <p>{userRecord.rank}</p>
-          <p>{userRecord.recordId}</p>
-          <p>{userRecord.seaTime}</p>
-          <p>{userRecord.status}</p>
-          <h3>Records are:</h3>
-          <hr />
-          {userRecord.serviceTimes &&
-            userRecord.serviceTimes.map((res, index) => (
-              <div key={index}>
-                <span>{res.time} | </span>
-                <span>{res.dateSignIn} | </span>
-                <span>{res.dateSignOff} | </span>
-                <span>{res.vesselNo} | </span>
-                <span>{res.vesselOwner} | </span>
-              </div>
-            ))}
-        </>
+        <div className="recordCard shadow1 m p  ">
+          <span className="badge">
+            {userRecord.name} - {userRecord.status}
+          </span>
+          <div className="row">
+            <div className="column">
+              <p>
+                <i className="fas fa-user-tag"></i> Record Id:{" "}
+                {userRecord.recordId}
+              </p>
+              <p>
+                <i className="far fa-id-badge"></i> CDN: {userRecord.cdn}
+              </p>
+            </div>
+            <div className="column">
+              <p>
+                <i className="fas fa-birthday-cake"></i> Date of Birth:{" "}
+                {userRecord.dateOfBirth}
+              </p>
+              <p>
+                <i className="fas fa-tag"></i> Rank: {userRecord.rank}
+              </p>
+            </div>
+          </div>
+          <p></p>
+          <h3>
+            <i className="fas fa-calendar-day"></i> Total Sea time:{" "}
+            {userRecord.seaTime} | Sea Records are:
+          </h3>
+          {userRecord.serviceTimes && (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className="hide-sm">#</th>
+                  <th>Vessel Reg no</th>
+                  <th>Vessel Owner</th>
+                  <th>From-To</th>
+                  <th>Time in days</th>
+                </tr>
+              </thead>
+              <tbody>{seaRecords}</tbody>
+            </table>
+          )}
+        </div>
       )}
     </>
   );
